11c78e1983729e8dbdfa8822096a5e88fdb1016d
Carsten Fortmann-Grote <grotec@evolbio.mpg.de>
2023-01-04 14:31:18 +0100

    Sucessfully testing all functions.

--- tests/test_tripser.py
+++ tests/test_tripser.py
@@ -1,35 +1,135 @@
 #!/usr/bin/env python
 """Tests for `tripser` package."""
+import os
+import shutil
+import sys
 
-import pytest
+import unittest
 from click.testing import CliRunner
 
+import tripser
 from tripser import cli
+from tripser.tripser import get_graph
+from tripser.tripser import parse_page
+from tripser.tripser import recursively_add
+from tripser.tripser import remove_terms
+from tripser.tripser import cleanup
+
+from rdflib import Graph, URIRef, RDFS
+import logging
+
+class TestTripser(unittest.TestCase):
+
+    def setUp(self):
+        """ Set up the test case."""
+
+        self._test_data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                       'test_data'
+                                                       ))
+        self.__thrashcan = []
+
+    def tearDown(self):
+        """ Tear down the test case."""
+
+        # Delete all items in the thrashcan.
+        for item in self.__thrashcan:
+            if os.path.isfile(item):
+                os.remove(item)
+            elif os.path.isdir(item):
+                shutil.rmtree(item)
+
+    def test_get_graph(self):
+        """ Test parsing a URL into a graph. """
+
+        page = "http://pflu.evolbio.mpg.de/web-services/content/v0.1/CDS/11845"
+
+        graph = get_graph(page)
+
+        self.assertIsInstance(graph, Graph)
+
+        # There should be 40 terms in this graph.
+        self.assertEqual(len(graph), 40)
+
+    def test_parse_page(self):
+        """ Test parsing a URL with members. """
+
+        cds_page = "http://pflu.evolbio.mpg.de/web-services/content/v0.1/CDS?page=3&limit=10"
+
+        cds_graph = parse_page(cds_page)
+
+        self.assertIsInstance(cds_graph, Graph)
+        self.assertEqual(len(cds_graph), 377)
+
+        # Get number of unique CDS subjects, should be 10.
+        self.assertEqual(
+            len(
+                [
+                    tr for tr in cds_graph.triples(
+                        (None,
+                         URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
+                         URIRef('http://www.sequenceontology.org/browser/current_svn/term/SO:0000316')
+                         )
+                    )
+                ]
+            ),
+            10
+        )
+
+    def test_recursively_add_feature(self):
+        """ Test recursively adding terms to a graph (no members). """
+        g = recursively_add(Graph(), ref=URIRef('http://pflu.evolbio.mpg.de/web-services/content/v0.1/CDS/11846'))
+
+        self.assertIsInstance(g, Graph)
+        self.assertEqual(len(g), 42)
+
+    def test_recursively_add_class(self):
+        """ Test recursively adding terms to a graph (with members)."""
+        g = recursively_add(Graph(), ref=URIRef('http://pflu.evolbio.mpg.de/web-services/content/v0.1/TRNA'))
+
+        self.assertEqual(len(g), 1732)
+
+        # Get number of unique TRNAs subjects, should be 66.
+        self.assertEqual(
+            len(
+                [
+                    tr for tr in g.triples(
+                        (None,
+                         URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
+                         URIRef('http://www.sequenceontology.org/browser/current_svn/term/SO:0000253')
+                         )
+                    )
+                ]
+            ),
+            66
+        )
+
+    def test_remove_terms(self):
+        """ Test removing multiple terms from a graph."""
+
+        graph = Graph().parse(os.path.join(self._test_data_dir, 'trna_messy.ttl'))
+
+        self.assertEqual(len(graph), 1732)
+
+        remove_terms(graph, (None,
+                             URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
+                             URIRef('http://www.sequenceontology.org/browser/current_svn/term/SO:0000253')
+                             )
+                     )
+
+        self.assertEqual(len(graph), 1666)
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
+    def test_cleanup(self):
+        """ Test cleaning up a graph."""
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+        logging.getLogger().setLevel(logging.DEBUG)
 
+        messy = Graph().parse(os.path.join(self._test_data_dir, 'trna_messy.ttl'))
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-    del response
+        self.assertEqual(len(messy), 1732)
+        cleanup(messy)
 
+        self.assertEqual(len(messy), 1725)
 
-def test_command_line_interface():
-    """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'pytripalserializer' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+if __name__ == "__main__":
+    unittest.main()
